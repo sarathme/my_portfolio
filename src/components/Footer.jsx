b04a47cbfc3e7e99ba0c6159998faf90e4008b44
Sarasraman <sarathsatheesh603@gmail.com>
2024-11-22 14:04:00 +0530

    Add footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import styles from "./Footer.module.css";
 
 function Footer() {
-  return <footer className={`${styles.footer} full-width`}>Footer</footer>;
+  return (
+    <footer className={`${styles.footer} full-width`}>
+      <p>
+        Designed And Developed with 🤍 by Sarasraman &copy;{" "}
+        {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
