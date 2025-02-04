a9959d65b7512b5f00ef454eae84a1bdb0a75c18
Sarasraman <87719028+sarathme@users.noreply.github.com>
2025-02-04 09:54:44 +0530

    Resume open in new tab

--- src/ui/Resume.jsx
+++ src/ui/Resume.jsx
@@ -2,7 +2,7 @@ import styles from "./Resume.module.css";
 function Resume() {
   return (
     <div className={styles.resume}>
-      <a href={import.meta.env.VITE_FIREBASE_TOKEN}>
+      <a href={import.meta.env.VITE_FIREBASE_TOKEN} target = "_blank">
         <button>Download Resume</button>
       </a>
     </div>
